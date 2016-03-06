5dd7be45287230cf1fda4c21b60253da4c633c9e
breno <brenosc2@hotmail.com>
2016-03-06 16:12:35 -0300

    Adição de busca por nome.

--- app/src/main/java/com/example/breno/seenme/TelaCadastrados.java
+++ app/src/main/java/com/example/breno/seenme/TelaCadastrados.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class TelaCadastrados extends Activity {
@@ -15,8 +16,8 @@ public class TelaCadastrados extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastrados);
-        final String[] consumido = new String[1];
 
+        SearchView searchView = (SearchView) findViewById(R.id.pesquisaItem);
         ListView listView = (ListView) findViewById(R.id.listaDeItensCadastrados);
         listView.setClickable(true);
         final RegraDeNegocioSingleton regraDeNegocioSingleton = RegraDeNegocioSingleton.getInstance();
@@ -26,86 +27,110 @@ public class TelaCadastrados extends Activity {
         arrayAdapter.notifyDataSetChanged();
         listView.setAdapter(arrayAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
-
-                AlertDialog.Builder firstBuilder = new AlertDialog.Builder(TelaCadastrados.this);
-                firstBuilder.setTitle(arrayAdapter.getItem(position).getNome());
-
-                if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).isConsumido()) {
-                    consumido[0] = " Sim";
-                } else {
-                    consumido[0] = " Não";
-                }
-
-                String textoComDetalhes = "Gênero: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getGenero() + "\n" +
-                        "Prioridade: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getPrioridade() + "\n" +
-                        "Ano: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getAno() + "\n" +
-                        "Autor: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getAutor() + "\n" +
-                        "Descrição: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getDescricao() + "\n" +
-                        "Idioma: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getIdioma() + "\n" +
-                        "Avaliação: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getAvaliacao() + "\n" +
-                        "Consumido:" + consumido[0] + "\n";
-
-                if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position) instanceof Filme) {
-                    textoComDetalhes = textoComDetalhes +
-                            "Duracao: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getDuracao() + "\n" +
-                            "Elenco: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getElenco() + "\n" +
-                            "Produtora: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getProdutora() + "\n" +
-                            "Formato: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getFormato() + "\n" +
-                            "Legenda: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getLegenda();
-                    firstBuilder.setIcon(R.drawable.iconefilme);
-                }
-
-                if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position) instanceof Livro) {
-                    textoComDetalhes = textoComDetalhes +
-                            "Editora: " + ((Livro) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getEditora() + "\n" +
-                            "Edição: " + ((Livro) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getEdicao();
-                    firstBuilder.setIcon(R.drawable.rsz_book);
-                }
-
-                firstBuilder.setMessage(textoComDetalhes);
-                final String labelEditar, labelToast;
-
-
-                if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).isConsumido()) {
-                    labelEditar = "Marcar como não consumido";
-                    labelToast = "Não!";
-
-                } else {
-                    labelEditar = "Marcar como consumido";
-                    labelToast = "Sim!";
-                }
-
-                firstBuilder.setNeutralButton(labelEditar, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).isConsumido()) {
-                            regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).setConsumido(false);
-                        } else {
-                            regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).setConsumido(true);
-                        }
-                        Toast.makeText(TelaCadastrados.this, "Status de consumido alterado para: " + labelToast, Toast.LENGTH_SHORT).show();
+            public boolean onQueryTextSubmit(String query) {
+                for (int i = 0; i < regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().size(); i++) {
+                    if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(i).getNome().equals(query)) {
+                        criaDialog(i, arrayAdapter, regraDeNegocioSingleton);
                     }
-                });
+                }
+                return false;
+            }
 
-                firstBuilder.setNegativeButton("Remover Item", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        arrayAdapter.remove(arrayAdapter.getItem(position));
-                        arrayAdapter.notifyDataSetChanged();
-                    }
-                });
-                firstBuilder.show();
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, final View view, final int position,
+                                                                    long id) {
+                                                criaDialog(position, arrayAdapter, regraDeNegocioSingleton);
+                                            }
+                                        }
+        );
     }
 
     @Override
     public void onBackPressed() {
         Intent retornaMainActivity = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(retornaMainActivity);
+    }
+
+    public void criaDialog(final int position, final CustomAdapter arrayAdapter, final RegraDeNegocioSingleton regraDeNegocioSingleton) {
+        String[] consumido = new String[1];
+
+        AlertDialog.Builder firstBuilder = new AlertDialog.Builder(TelaCadastrados.this);
+        firstBuilder.setTitle(arrayAdapter.getItem(position).getNome());
+
+        if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).isConsumido()) {
+            consumido[0] = " Sim";
+        } else {
+            consumido[0] = " Não";
+        }
+
+        String textoComDetalhes = "Gênero: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getGenero() + "\n" +
+                "Prioridade: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getPrioridade() + "\n" +
+                "Ano: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getAno() + "\n" +
+                "Autor: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getAutor() + "\n" +
+                "Descrição: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getDescricao() + "\n" +
+                "Idioma: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getIdioma() + "\n" +
+                "Avaliação: " + regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).getAvaliacao() + "\n" +
+                "Consumido:" + consumido[0] + "\n";
+
+        if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position) instanceof Filme) {
+            textoComDetalhes = textoComDetalhes +
+                    "Duracao: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getDuracao() + "\n" +
+                    "Elenco: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getElenco() + "\n" +
+                    "Produtora: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getProdutora() + "\n" +
+                    "Formato: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getFormato() + "\n" +
+                    "Legenda: " + ((Filme) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getLegenda();
+            firstBuilder.setIcon(R.drawable.iconefilme);
+        }
+
+        if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position) instanceof Livro) {
+            textoComDetalhes = textoComDetalhes +
+                    "Editora: " + ((Livro) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getEditora() + "\n" +
+                    "Edição: " + ((Livro) regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position)).getEdicao();
+            firstBuilder.setIcon(R.drawable.rsz_book);
+        }
+
+        firstBuilder.setMessage(textoComDetalhes);
+        final String labelEditar, labelToast;
+
+
+        if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).isConsumido()) {
+            labelEditar = "Marcar como não consumido";
+            labelToast = "Não!";
+
+        } else {
+            labelEditar = "Marcar como consumido";
+            labelToast = "Sim!";
+        }
+
+        firstBuilder.setNeutralButton(labelEditar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if (regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).isConsumido()) {
+                    regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).setConsumido(false);
+                } else {
+                    regraDeNegocioSingleton.getListaDeItensSingleton().getListaDeItensCulturais().get(position).setConsumido(true);
+                }
+                Toast.makeText(TelaCadastrados.this, "Status de consumido alterado para: " + labelToast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        firstBuilder.setNegativeButton("Remover Item", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                arrayAdapter.remove(arrayAdapter.getItem(position));
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+        firstBuilder.show();
     }
 }
